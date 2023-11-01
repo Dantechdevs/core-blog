84835962f6d54ccc273d30d7cb16698f4e0fed96
kinyua001 <murimicodes@gmail.com>
2023-11-01 18:27:22 +0300

    add update to advocates

--- routes/advocates.php
+++ routes/advocates.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Livewire\AdvocatesUpdate;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\AdvocatesIndex;
 use App\Livewire\AdvocatesCreate;
 
 
 
-  Route::prefix('advocates')->group(function () {
-        Route::get('index', AdvocatesIndex::class)->name('admin.advocates.index');
-        Route::get('N/create', AdvocatesCreate::class)->name('admin.advocates.create');
-    });
+Route::prefix('advocates')->group(function () {
+    Route::get('index', AdvocatesIndex::class)->name('admin.advocates.index');
+    Route::get('N/create', AdvocatesCreate::class)->name('admin.advocates.create');
+    Route::get('A/{advocate}/update', AdvocatesUpdate::class)->name('admin.advocates.update');
+});
